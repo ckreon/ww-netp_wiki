a7c68f72685055af052572a50ede41cd66f2b1ca
Corey Kozlowski <ckreon@gmail.com>
2016-04-13 19:53:16 -0700

    Ready site for production

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,8 +2,8 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_3a7b766da08b_key -iv $encrypted_3a7b766da08b_iv -in _deploy/net_wiki_deploy.enc -out _deploy/net_wiki_deploy -d
-rm _deploy/net_wiki_deploy.enc # Don't need it anymore
+openssl aes-256-cbc -K $encrypted_3a7b766da08b_key -iv $encrypted_3a7b766da08b_iv -in _deploy/netp_wiki_deploy.enc -out _deploy/netp_wiki_deploy -d
+rm _deploy/netp_wiki_deploy.enc # Don't need it anymore
 chmod 600 _deploy/netp_wiki_deploy
 mv _deploy/netp_wiki_deploy ~/.ssh/netp_wiki_deploy
 
