b75d65e3ce2c10d435a015e5af20d751dbd2dd2b
Corey Kozlowski <ckreon@gmail.com>
2016-04-13 20:15:12 -0700

    Ready site for production

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -8,6 +8,6 @@ node makepdf.js
 bundle exec jekyll build
 
 # Compress assets with Zopfli
-zopfli/zopfli --i1000 _site/*.xml _site/*.html _site/**/*.html _site/assets/css/*.css _site/assets/pdf/*.pdf _site/assets/fonts/*
+_zopfli/zopfli --i1000 _site/*.xml _site/*.html _site/**/*.html _site/assets/css/*.css _site/assets/pdf/*.pdf _site/assets/fonts/*
 
 # zopfli/zopfli --i1000 _site/**/*.html _site/*.html  _site/**/*.css _site/*.css _site/**/*.js _site/*.js _site/**/*.xml _site/*.xml _site/assets/pdf/* _site/assets/fonts/*
