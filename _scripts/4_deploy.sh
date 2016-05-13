163bf7b5462b6c6f85b4c24c87934fb0d12170df
Corey Kozlowski <ckreon@gmail.com>
2016-05-13 04:10:57 -0700

    General cleanup and update

--- _scripts/4_deploy.sh
+++ _scripts/4_deploy.sh
@@ -3,28 +3,29 @@ set -x
 
 if [ $TRAVIS_PULL_REQUEST == false ] ; then
 
-  # Deploy to Production Server if branch is master
-  if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Compress assets with Zopfli
-    _zopfli/zopfli --i1000 _site/*.xml _site/*.html _site/**/*.html _site/assets/*
+	# Deploy to Production Server if branch is master
+	if [ $TRAVIS_BRANCH == 'master' ] ; then
+		# Compress assets with Zopfli
+		_zopfli/zopfli --i1000 _site/*.xml _site/*.html _site/**/*.html _site/assets/*
 
-    # Start SSH Agent
-    eval `ssh-agent -s`
-    ssh-add ~/.ssh/netp_wiki_deploy
+		# Start SSH Agent
+		eval `ssh-agent -s`
+		ssh-add ~/.ssh/netp_wiki_deploy
 
-    # Initialize a new git repo in _site, and push it to our server.
-    cd _site
-    git init
-        
-    git remote add deploy "deploy@159.203.216.201:/var/www/netp_wiki"
-    git config user.name "Travis CI"
-    git config user.email "ckreon+TravisCI@gmail.com"
-    
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
-  else
-    echo "Not deploying, branch is not master."
-  fi
+		# Initialize a new git repo in _site, and push it to our server.
+		cd _site
+		git init
+		    
+		git remote add deploy "deploy@159.203.216.201:/var/www/netp_wiki"
+		git config user.name "Travis CI"
+		git config user.email "ckreon+TravisCI@gmail.com"
+
+		git add .
+		git commit -m "Deploy"
+		git push --force deploy master
+		
+		else
+		echo "Not deploying, branch is not master."
+	fi
 
 fi
